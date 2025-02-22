c6951d397686c5cbe4bdf8584e8d2922dab51cc4
knakatasf <knakata@dons.usfca.edu>
2025-02-21 17:19:41 -0800

    Refactored

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,7 @@
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON your_database_name.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 
 CREATE TABLE books (
